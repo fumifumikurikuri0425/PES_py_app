2b152ebf793d266fa7603e2d5ffcb9ae208abda6
fumiokurihara <fumio.kurihara@me.com>
2021-05-28 18:53:38 +0900

    start project

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 from bokeh.embed import components
 from bokeh.resources import INLINE
+from bokeh.embed import json_item
 
 from make_plot2 import make_plot
 from graph2 import make_data
@@ -16,39 +19,192 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-templates = Jinja2Templates(directory='templates')
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
-async def read_item(request:Request,
-                    #intervalのデフォルトは0.05
-                    interval: float = 0.05,
-                    x: float = -0.75, y: float = 0.55,
-                    tone: int = 20, check: int = 0,
-                    step: float = 0.01):
+async def read_item_1(
+    request: Request,
+    # intervalのデフォルトは0.05
+    interval: float = 0.05,
+    x: float = -0.75,
+    y: float = 0.55,
+    tone: int = 20,
+    check: int = 0,
+    step: float = 0.01,
+    xmin: float = -2.5,
+    xmax: float = 1.5,
+    ymin: float = -1,
+    ymax: float = 3,
+):
     # グラフを作成する。
     print("colortone:", tone)
-    print("interval:",interval)
-    print("x=",x,"y=",y)
-    print("step=",step)
+    print("interval:", interval)
+    print("x=", x, "y=", y)
+    print("step=", step)
 
     js_resources = INLINE.render_js()
     css_resources = INLINE.render_css()
 
     check_value = check
-    print("check_value:",check_value)
-    make_data(x,y,check_value,step)
-    fig = make_plot(tone,interval)
+    print("check_value:", check_value)
+    make_data(x, y, check_value, step)
+    fig = make_plot(tone, interval, xmin, xmax, ymin, ymax)
 
     script, div = components(fig, INLINE)
     # print(script)
     # print(div)
 
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "js_resources": js_resources,
+            "css_resources": css_resources,
+            "plot_div": div,
+            "plot_script": script,
+            "step": step,
+            "x": x,
+            "y": y,
+            "tone": tone,
+            "check": check,
+            "xmin": xmin,
+            "xmax": xmax,
+            "ymin": ymin,
+            "ymax": ymax,
+        },
+    )
 
-    return templates.TemplateResponse("index.html", {"request":request,
-                                                      "js_resources":js_resources,
-                                                      "css_resources": css_resources,
-                                                      "plot_div": div, "plot_script": script,
-                                                      "step": step, "x":x, "y":y,"tone":tone, "check":check})
+
+@app.get("/api2")
+async def read_item_2(
+    request: Request,
+    # intervalのデフォルトは0.05
+    interval: float = 0.1,
+    x: float = -0.75,
+    y: float = 0.55,
+    tone: int = 5,
+    check: int = 0,
+    step: float = 0.01,
+    xmin: float = -2.5,
+    xmax: float = 1.5,
+    ymin: float = -1,
+    ymax: float = 3,
+):
+    # グラフを作成する。
+    print("colortone:", tone)
+    print("interval:", interval)
+    print("x=", x, "y=", y)
+    print("step=", step)
+
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
+
+    check_value = check
+    print("check_value:", check_value)
+    make_data(x, y, check_value, step)
+    fig = make_plot(tone, interval, xmin, xmax, ymin, ymax)
+
+    script, div = components(fig, INLINE)
+    # print(script)
+    # print(div)
+
+    retval = {
+        "js_resources": js_resources,
+        "css_resources": css_resources,
+        "plot_div": div,
+        "plot_script": script,
+        "step": step,
+        "x": x,
+        "y": y,
+        "tone": tone,
+        "check": check,
+        "xmin": xmin,
+        "xmax": xmax,
+        "ymin": ymin,
+        "ymax": ymax,
+    }
+
+    return retval
+
+
+@app.get("/api3")
+async def read_item_3(
+    request: Request,
+    # intervalのデフォルトは0.05
+    interval: float = 0.1,
+    x: float = -0.75,
+    y: float = 0.55,
+    tone: int = 5,
+    check: int = 0,
+    step: float = 0.01,
+    xmin: float = -2.5,
+    xmax: float = 1.5,
+    ymin: float = -1,
+    ymax: float = 3,
+):
+    # グラフを作成する。
+    print("colortone:", tone)
+    print("interval:", interval)
+    print("x=", x, "y=", y)
+    print("step=", step)
+
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
+
+    check_value = check
+    print("check_value:", check_value)
+    make_data(x, y, check_value, step)
+    fig = make_plot(tone, interval, xmin, xmax, ymin, ymax)
+
+    script, div = components(fig, INLINE)
+    # print(script)
+    # print(div)
+
+    retval = {
+        "js_resources": js_resources,
+        "css_resources": css_resources,
+        "plot_div": div,
+        "plot_script": script,
+        "step": step,
+        "x": x,
+        "y": y,
+        "tone": tone,
+        "check": check,
+        "xmin": xmin,
+        "xmax": xmax,
+        "ymin": ymin,
+        "ymax": ymax,
+    }
+
+    return json_item(fig, "myplot")
+
+
+class Params(BaseModel):
+    interval: float = 0.1
+    x: float = -0.75
+    y: float = 0.55
+
+
+@app.post("/api4")
+def post_api4(params: Params):
+
+    return {"test": params}
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 if __name__ == "__main__":
--- make_plot2.py
+++ make_plot2.py
@@ -7,41 +7,45 @@ import sys
 from skimage import measure
 from bokeh.plotting import figure, show
 from bokeh import palettes
-from bokeh.models import (ColorBar, FixedTicker, LinearColorMapper, PrintfTickFormatter)
+from bokeh.models import ColorBar, FixedTicker, LinearColorMapper, PrintfTickFormatter
 from bokeh.resources import CDN
 
 
-A=[-200,-100,-170,15]
-a=[-1,-1,-6.5,0.7]
-b=[0,0,11,0.6]
-c=[-10,-10,-6.5,0.7]
-X=[1,0,-0.5,-1]
-Y=[0,0.5,1.5,1]
+A = [-200, -100, -170, 15]
+a = [-1, -1, -6.5, 0.7]
+b = [0, 0, 11, 0.6]
+c = [-10, -10, -6.5, 0.7]
+X = [1, 0, -0.5, -1]
+Y = [0, 0.5, 1.5, 1]
 
-def Exy(x,y):
+
+def Exy(x, y):
     Exy = 0
     for i in range(4):
-        Exy += A[i]*math.exp(a[i]*((x - X[i])**2) + b[i]*(x - X[i])*(y - Y[i]) + c[i]*((y - Y[i])**2))
+        Exy += A[i] * math.exp(
+            a[i] * ((x - X[i]) ** 2)
+            + b[i] * (x - X[i]) * (y - Y[i])
+            + c[i] * ((y - Y[i]) ** 2)
+        )
     return Exy
 
 
-def make_plot(color_tone,interval):
+def make_plot(color_tone, interval, xmin, xmax, ymin, ymax):
 
-    #x,y,zの配列を生成
-    np.set_printoptions(precision=6, floatmode='fixed', suppress=True)
+    # x,y,zの配列を生成
+    np.set_printoptions(precision=6, floatmode="fixed", suppress=True)
 
     X_list = []
     Y_list = []
     Z_list = []
 
-    for i, x in enumerate(np.arange(-2.5, 1.51, interval)):
-        for j, y in enumerate(np.arange(-1.0, 3.01, interval)):
+    for i, x in enumerate(np.arange(xmin, xmax + 0.01, interval)):
+        for j, y in enumerate(np.arange(ymin, ymax + 0.01, interval)):
             X_list.append(float(x))
             Y_list.append(float(y))
-            MBP = Exy(x,y)
+            MBP = Exy(x, y)
             MBP = "{:.6f}".format(MBP)
             Z_list.append(float(MBP))
-
 
     def get_meshgrid_from_xyzArray(xar, yar, zar):
         # mx, my, mz : in meshgrid
@@ -61,42 +65,53 @@ def make_plot(color_tone,interval):
         mx, my = np.meshgrid(xuniq, yuniq)
         return mx, my, mz
 
-    X_list_meshed, Y_list_meshed, Z_list_meshed = get_meshgrid_from_xyzArray(X_list, Y_list, Z_list)
+    X_list_meshed, Y_list_meshed, Z_list_meshed = get_meshgrid_from_xyzArray(
+        X_list, Y_list, Z_list
+    )
 
-    p = figure(tooltips=[("X", "$x"), ("Y", "$y"), ("value", "@image")],width=700,height=500)
+    p = figure(
+        tooltips=[("X", "$x"), ("Y", "$y"), ("value", "@image")], width=700, height=500
+    )
     p.x_range.range_padding = p.y_range.range_padding = 0
 
-    exp_cmap = LinearColorMapper(palette=palettes.inferno(color_tone),
-                                low = -147,
-                                high = 100)
+    exp_cmap = LinearColorMapper(
+        palette=palettes.inferno(color_tone), low=-147, high=100
+    )
 
-
-    p.image (image=[Z_list_meshed], x=-2.5, y=-1.0, dw=4, dh=4, color_mapper=exp_cmap,  level="image")
+    p.image(
+        image=[Z_list_meshed],
+        x=xmin,
+        y=ymin,
+        dw=xmax - xmin,
+        dh=ymax - ymin,
+        color_mapper=exp_cmap,
+        level="image",
+    )
     p.grid.grid_line_width = 0.5
-
 
     levels = np.linspace(-147, 100, color_tone)
 
-    color_bar = ColorBar(color_mapper=exp_cmap, major_label_text_font_size='8pt', ticker=FixedTicker(ticks=levels),
-                        formatter=PrintfTickFormatter(format='%.2f'), label_standoff=6, border_line_color=None, location=(0, 0))
+    color_bar = ColorBar(
+        color_mapper=exp_cmap,
+        major_label_text_font_size="8pt",
+        ticker=FixedTicker(ticks=levels),
+        formatter=PrintfTickFormatter(format="%.2f"),
+        label_standoff=6,
+        border_line_color=None,
+        location=(0, 0),
+    )
 
-    p.add_layout(color_bar, 'right')
+    p.add_layout(color_bar, "right")
 
     for level in levels:
         contours = measure.find_contours(Z_list_meshed, level)
         for contour in contours:
 
-            x = contour[:,1]/20 - 2.5
-            y = contour[:,0]/20 - 1.0
-            p.line(x, y, color='grey', line_width=1,alpha=0.7)
+            x = contour[:, 1] / 20 + xmin
+            y = contour[:, 0] / 20 + ymin
+            p.line(x, y, color="grey", line_width=1, alpha=0.7)
 
-
-    data_set = np.loadtxt(
-            fname="graph2.csv",
-            dtype="float",
-            delimiter=","
-        )
-
+    data_set = np.loadtxt(fname="graph2.csv", dtype="float", delimiter=",")
 
     p_x = []
     p_y = []
@@ -104,12 +119,9 @@ def make_plot(color_tone,interval):
     for data in data_set:
         p_x.append(data[0])
         p_y.append(data[1])
-    #x_vol = len(p_x)
+    # x_vol = len(p_x)
     # y_vol = len(p_y)
 
-    p.line(p_x,p_y,line_width=2,
-        color="cyan",
-        alpha=0.5
-        )
+    p.line(p_x, p_y, line_width=2, color="cyan", alpha=0.5)
 
     return p
